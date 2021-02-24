5ec583191ff4c98659ca04cf4466562e2a778325
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-02-23 19:02:04 -0500

    Update AsignaturaExtracurricular.java

--- Control Notas Educacion/src/modelo/AsignaturaExtracurricular.java
+++ Control Notas Educacion/src/modelo/AsignaturaExtracurricular.java
@@ -5,10 +5,61 @@
  */
 package modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author sagov8
  */
 public class AsignaturaExtracurricular {
+   
+    private String codigoMateria;
+    private String nombreAsignatura;
+    private ArrayList notas;
+    private float promedio;
+
+    public AsignaturaExtracurricular(String codigoMateria, String nombreAsignatura, ArrayList notas, float promedio) {
+        this.codigoMateria = codigoMateria;
+        this.nombreAsignatura = nombreAsignatura;
+        this.notas = notas;
+        this.promedio = promedio;
+    }
+
+    public AsignaturaExtracurricular() {
+    
+    }
+
+    public String getCodigoMateria() {
+        return codigoMateria;
+    }
+
+    public void setCodigoMateria(String codigoMateria) {
+        this.codigoMateria = codigoMateria;
+    }
+
+    public String getNombreAsignatura() {
+        return nombreAsignatura;
+    }
+
+    public void setNombreAsignatura(String nombreAsignatura) {
+        this.nombreAsignatura = nombreAsignatura;
+    }
+
+    public ArrayList getNotas() {
+        return notas;
+    }
+
+    public void setNotas(ArrayList notas) {
+        this.notas = notas;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+    
     
 }
