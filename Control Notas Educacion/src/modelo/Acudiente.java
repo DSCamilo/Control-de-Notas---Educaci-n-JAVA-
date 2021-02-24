cf17675dd5522f55c6f4f38beacef840ec617952
Camilo Benitez <camilobenitez@unimayor.edu.co>
2021-02-23 19:03:02 -0500

    Atributos Clase Persona , Aplicar herencia clase persona y acudiente
    Herencia clase Estudiante y Acudiente

--- Control Notas Educacion/src/modelo/Acudiente.java
+++ Control Notas Educacion/src/modelo/Acudiente.java
@@ -2,10 +2,20 @@
 package modelo;
 
 
-public class Acudiente {
+public class Acudiente extends Persona {
     
     
    private String codigoAcudiente;
+
+    public String getCodigoAcudiente() {
+        return codigoAcudiente;
+    }
+
+    public void setCodigoAcudiente(String codigoAcudiente) {
+        this.codigoAcudiente = codigoAcudiente;
+    }
+   
+   
     
     
     public void ConsultarNotas(){
--- Control Notas Educacion/src/modelo/Persona.java
+++ Control Notas Educacion/src/modelo/Persona.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author Usuario
- */
+
 public class Persona {
+    
+    
+    private String nombre;
+    private String apellido;
+    private int telefono;
+    private String correoElectronico;
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+    
     
 }
