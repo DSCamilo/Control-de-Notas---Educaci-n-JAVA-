43d98dc978e3d886e88b4ef2ce7b4279c51b8d28
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-02-23 19:33:05 -0500

    Create Login.java

---
+++ Control Notas Educacion/src/modelo/Login.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author sagov8
+ */
+public interface Login {
+    public void iniciarSesion();
+    public void registrarUsuario();
+}
