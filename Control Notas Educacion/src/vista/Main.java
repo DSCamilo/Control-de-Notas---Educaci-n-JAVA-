f60926a167f3d692d367a288c3548b951f0946b6
Camilo Benitez <camilobenitez@unimayor.edu.co>
2021-02-24 21:13:17 -0500

    menu main opciones

--- Control Notas Educacion/src/vista/Main.java
+++ Control Notas Educacion/src/vista/Main.java
@@ -1,6 +1,7 @@
 package vista;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import modelo.Acudiente;
 import modelo.AsignaturaExtra;
 import modelo.Carnet;
@@ -9,10 +10,10 @@ import modelo.Estudiante;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
 
         Carnet carnet1 = new Carnet();
-        
-        
+
         Estudiante estudiante1 = new Estudiante("001", "03/01/2001");
 
         estudiante1.setNombre("Magnolia");
@@ -23,26 +24,47 @@ public class Main {
         notasEstudiante.add(4.0);
         notasEstudiante.add(5.0);
         notasEstudiante.add(4.2);
-        
+
         AsignaturaExtra asignatura1 = new AsignaturaExtra("00001", "Musica", notasEstudiante, 0);
-        
+
         asignatura1.setPromedio(asignatura1.calcularPromedio(notasEstudiante));
-        
-        ArrayList<AsignaturaExtra> asignaturas=new ArrayList<>();
+
+        ArrayList<AsignaturaExtra> asignaturas = new ArrayList<>();
         asignaturas.add(asignatura1);
-        
+
         Acudiente acudiente1 = new Acudiente("123", estudiante1);
         acudiente1.setNombre("Patricia");
         acudiente1.setApellido("Pérez");
-        
-        System.out.println("Bienvenido al sistema de control de notas +"
-                + "\n¿Es usted acudiente? Marque 1 +"
-                + "\n¿Es usted estudiante? Marque 2");
-        System.out.println("El acudiente del estudiante " + estudiante1.getNombre() + " es " +
-                "s");
-        
-        
 
-        
+        System.out.println("▓▓Bienvenido al sistema de control de notas▓▓"
+                + "\n¿Es usted acudiente? Marque 1 "
+                + "\n¿Es usted estudiante? Marque 2");
+
+        int opcionUsuario;
+
+        opcionUsuario = t.nextInt();
+
+        if (opcionUsuario == 1) {
+            System.out.println("░░Bienvenido al sistema de Notas░░");
+            System.out.println("");
+            System.out.println("Opciones del Sistema, que opcion desea realizar");
+            System.out.println("1. Consultar notas");
+            
+  
+      
+
+        } else if (opcionUsuario==2) {
+            System.out.println("░░Bienvenido al sistema de Notas░░"+""+estudiante1.getNombre());
+            System.out.println("");
+            System.out.println("Opciones del Sistema, que opcion desea realizar");
+            System.out.println("1. Agregar Asiganatura");
+            System.out.println("2. Consultar Notas");
+
+        } else {
+
+            System.out.println("║Utilice una opcion valida║");
+
+        }
+
     }
 }
