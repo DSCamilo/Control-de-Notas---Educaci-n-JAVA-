254befbff0517fc0b97c9b03515e378ae5c4e560
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-02-24 22:18:51 -0500

    acudiente

--- Control Notas Educacion/src/modelo/Acudiente.java
+++ Control Notas Educacion/src/modelo/Acudiente.java
@@ -28,7 +28,8 @@ public class Acudiente extends Persona implements MenuConsulta{
     public void setCodigoAcudiente(String codigoAcudiente) {
         this.codigoAcudiente = codigoAcudiente;
     } 
-
+    
+    //Interfaz
     @Override
     public void consultarNota() {
         for(int i = 0 ; i < estudiante.getAsignaturas().size(); i++){
